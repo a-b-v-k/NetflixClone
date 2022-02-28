c9cc09dc9f140339d43e69e8935c1eda85039aee
a-b-v-k <62746809+a-b-v-k@users.noreply.github.com>
2022-02-28 12:38:54 +0530

    Update firebase.js

--- Admin/src/firebase.js
+++ Admin/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/compat/app'
 import "firebase/compat/storage"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB0ffPKJqac9L0fPmZhEA4WlrI-p5TH6oA",
-    authDomain: "netflix-e1a36.firebaseapp.com",
-    projectId: "netflix-e1a36",
-    storageBucket: "netflix-e1a36.appspot.com",
-    messagingSenderId: "478750858204",
-    appId: "1:478750858204:web:b169fa73252a64c80b8d19",
-    measurementId: "G-HVTWRVHV71"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxx",
+    measurementId: "xxxxxxxxxxxxxx"
 };
 
 firebase.initializeApp(firebaseConfig);
